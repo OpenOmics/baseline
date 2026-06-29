150459a0e6bebff4754f0f5b2c95529266f1cbfe
skchronicles <kuhnsa3@gmail.com>
2026-06-29 17:26:00 -0400

    refactor: base snakemake workflow template.

--- workflow/scripts/utils.sh
+++ workflow/scripts/utils.sh
@@ -6,19 +6,19 @@ function fatal() { err "$@"; exit 1; }
 function abspath() { readlink -e "$1"; }
 
 function retry() {
-  # Tries to run a cmd 5 times before failing 
+  # Tries to run a cmd 5 times before failing
   # If a command is successful, it will break out of attempt loop
-  # Failed attempts are padding with the following exponential 
+  # Failed attempts are padding with the following exponential
   # back-off strategy {4, 16, 64, 256, 1024} in seconds
-  # @INPUTS "$@"" = cmd to run 
+  # @INPUTS "$@"" = cmd to run
   # @CALLS fatal() if command cannot be run in 5 attempts
   local n=1
   local max=5
   local attempt=true # flag for while loop
   while $attempt; do
-    # Attempt command and break if successful 
+    # Attempt command and break if successful
     "$@" && attempt=false || {
-      # Try again up to 5 times 
+      # Try again up to 5 times
       if [[ $n -le $max ]]; then
         err "Command failed: $@"
         delay=$(( 4**$n ))
